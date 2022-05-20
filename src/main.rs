413256f2125d4b5ab875b1d382d8c6a882ee2ea8
the6p4c <me@doggirl.gay>
2022-05-21 03:21:43 +1000

    load any day's puzzle

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use chrono::{Date, TimeZone, Utc};
+use chrono::{TimeZone, Utc};
 use itertools::izip;
 use serde::Deserialize;
 use std::error::Error;
@@ -44,11 +44,15 @@ impl OfficialData {
         Ok(Self::from_json(&dictionary_json, &game_json))
     }
 
-    fn from_web() -> reqwest::Result<Self> {
-        const BASE_URL: &str = "https://www.andrewt.net/puzzles/cell-tower";
-
+    fn from_web_today() -> reqwest::Result<Self> {
         let epoch = Utc.ymd(2022, 05, 06).and_hms(0, 0, 0);
         let puzzle_id = Utc::now().signed_duration_since(epoch).num_days() + 1;
+
+        Self::from_web(puzzle_id)
+    }
+
+    fn from_web(puzzle_id: i64) -> reqwest::Result<Self> {
+        const BASE_URL: &str = "https://www.andrewt.net/puzzles/cell-tower";
 
         let client = reqwest::blocking::Client::new();
         let dictionary_json = client
@@ -122,20 +126,29 @@ impl OfficialData {
 #[structopt(about = "a terminal-based clone of the cell tower puzzle game")]
 enum Paperbark {
     Today,
+    Day { puzzle_id: u64 },
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
     let opt = Paperbark::from_args();
-    let game = match opt {
+    match opt {
         Paperbark::Today => {
-            let official_data = OfficialData::from_web()?;
+            let official_data = OfficialData::from_web_today()?;
             let board = official_data.board();
             let ruleset = official_data.ruleset();
 
             let game = Game::<Color>::new(&board, &ruleset);
             ui::run(game)?;
         }
-    };
+        Paperbark::Day { puzzle_id } => {
+            let official_data = OfficialData::from_web(puzzle_id as i64)?;
+            let board = official_data.board();
+            let ruleset = official_data.ruleset();
+
+            let game = Game::<Color>::new(&board, &ruleset);
+            ui::run(game)?;
+        }
+    }
 
     Ok(())
 }
